98c4165866685da98e87e93fb0c06772a6a8c1a6
Luciana Romina Schmidt <luciana.rschmidt@gmail.com>
2023-05-23 20:16:07 -0300

    "modificaciones varias"

---
+++ script.js
@@ -0,0 +1,47 @@
+const textArea = document.querySelector(".ingresar-texto");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e","enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e","enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
+
+var botonCopiar = document.querySelector("#boton-copiar");
+botonCopiar.onclick = copiar;
+function btnCopiar(){
+    var contenido = document.querySelector("#copiar-texto");
+    contenido.select();
+    document.execCommand("copy");
+}
+
+
